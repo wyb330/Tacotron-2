f5c60a082083f96ade6788e277dc86c0d8f08c36
wyb330 <33001941+wyb330@users.noreply.github.com>
2018-06-27 00:03:56 +0900

    한국어 음성합성 기능 추가

--- preprocess.py
+++ preprocess.py
@@ -25,11 +25,11 @@ def preprocess(args, input_folders, out_dir, hparams):
 
 def write_metadata(metadata, out_dir):
     with open(os.path.join(out_dir, 'train.txt'), 'w', encoding='utf-8') as f:
+        metadata = [m for m in metadata if m]
         for m in metadata:
-            if m:
-                f.write('|'.join([str(x) for x in m]) + '\n')
-    mel_frames = sum([int(m[4]) for m in metadata if m])
-    timesteps = sum([int(m[3]) for m in metadata if m])
+            f.write('|'.join([str(x) for x in m]) + '\n')
+    mel_frames = sum([int(m[4]) for m in metadata])
+    timesteps = sum([int(m[3]) for m in metadata])
     sr = hparams.sample_rate
     hours = timesteps / sr / 3600
     print('Write {} utterances, {} mel frames, {} audio timesteps, ({:.2f} hours)'.format(
